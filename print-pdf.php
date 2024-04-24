0cfb5220a577ff5276c2964ed03cf4b30b0c161f
popcouncilconsulting <101557954+popcouncilconsulting@users.noreply.github.com>
2024-04-24 17:42:12 +0530

    Add files via upload

---
+++ print-pdf.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+  
+<div class="container mt-5">
+
+	<div class="row">
+		
+	</div>
+
+  <div class="row" >
+	<!--<div class="col-sm-12" id="pdftablehdr" style="display:none;"><h1>This is test Header</h1></div> -->
+    <div class="col-sm-12 text-center">
+      <h3>Investment Declaration Form</h3>
+    </div>
+    
+	<div class="col-sm-12">
+		<table class="table table-bordered" style="border: 1px solid black;">
+			<tr>
+			  <th colspan="2">Name and Address of Employee:</th>
+			  <td colspan="2"></td>
+			</tr>
+			<tr>
+			  <th colspan="2">PAN No. of employee:</th>
+			  <td colspan="2"></td>
+			</tr>
+			<tr>
+			  <th colspan="2">Financial Year:</th>
+			  <td colspan="2">2023-2024</td>
+			</tr>
+			<tr>
+			  <th colspan="2">Period:</th>
+			  <td colspan="2">01 April 2023 to 31 March 2024</td>
+			</tr>
+			
+			<tr>
+			  <th colspan="4">Details of claims and evidence thereof</th>
+			</tr>
+			<tr>
+			  <th>SI No.</th>
+			  <th>Nature of claim</th>
+			  <th>Amount (Rs.)</th>
+			  <th>Evidence / Particulars (enclosed the supporting documents)</th>
+			</tr>
+			<tr>
+			  <td>1</td>
+			  <td>House Rent Allowance:</td>
+			  <td></td>
+			  <td>1) Copy of Rent Agreement</td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(i) Rent paid to the landlord</td>
+			  <td></td>
+			  <td>2) Rent Receipts</td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(ii) Name of the landlord</td>
+			  <td></td>
+			  <td>3) Scanned Copy of PAN of the landlord.</td>
+			</tr>
+			
+			<tr>
+			  <td></td>
+			  <td>(iii) Address of the landlord</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(iv) Permanent Account Number of the landlord</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <th>Total Amount (Rent amount)</th>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td colspan="4">Note: Permanent Account Number shall be furnished if the aggregate rent paid during the previous year exceeds one lakh rupees</td>
+			</tr>
+			<tr>
+			  <td>2</td>
+			  <th>Deduction of interest on Housing loan:</th>
+			  <td></td>
+			  <td>Interest certificate form bank/FI to be submitted</td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(i) Interest payable/paid to the Bank/FI</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(ii) Name of the Bank/FI</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(iii) Address of the Bank/FI</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <th>Total Home loan Interest amount</th>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td colspan="4">Note: 1) Provisional Certificate to be shared (in April as declaration proof). 2) Actual Statement to be submitted (during March) to be final interest paid amount consideration.</td>
+			</tr>
+			<tr>
+			  <td>3</td>
+			  <td>Deduction under Chapter VI-A <br> (i) Section 80C. Maximum Limit Rs. 1.5 lakhs per Financial Year <br>Type of Investments: e.g. LIC, PPF, Mutual Fund, Principle Amount paid against Home Loan, Children Tuition Fees etc. </td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			
+			<tr>
+			  <td></td>
+			  <td>(1)</td>
+			  <td></td>
+			  <td>Note: Enclosed the receipts as supporting.</td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(2)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(3)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(4)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(5)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(6)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(7)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <th>Total Amount (u/s 80 C)</th>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <th>(ii) Section 80CCD (NPS. Max Tax Rebate Limit Rs. 50,000/-)</th>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>NPS (National Pension System)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td>4</td>
+			  <th>Health Insurance premium (u/s 80 D) maximum limit upto 25 thousands</th>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(1)</td>
+			  <td></td>
+			  <td>Note: Enclosed the receipts as supporting.</td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td>(2)</td>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td></td>
+			  <th>Total Amount (u/s 80 D)</th>
+			  <td></td>
+			  <td></td>
+			</tr>
+			<tr>
+			  <td colspan="4">Verification</td>
+			</tr>
+			<tr>
+			  <td colspan="4">I,........................................., son/daughter of ....................................., do hereby certify that the information given above is complete and correct.</td>
+			  
+			</tr>
+			<tr>
+			  <td colspan="2">Place: ......................................................................</td>
+			  <td rowspan="4" colspan="2">(Signature of the employee) <br>Full Name:....................</td>
+			</tr>
+			<tr>
+			  <td colspan="2">Date: ........................................................................</td>
+			</tr>
+			<tr>
+			  <td colspan="2">Designation: ..........................................................</td>
+			</tr>
+		  </table>
+	</div>
+	
+  </div>
+  <a href="javascript:void()" onclick="window.print()" id="print-btn" class="btn btn-sm border-warning mb-4 float-end" >Print</a>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script>
+	// $("#print-btn").on("click",function(){
+		// $("#print-btn").remove();
+	// })
+</script>
+
+</body>
+
+</html>
